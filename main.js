94f1ed99adec23417e153c118af126a4bddff09d
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-06-08 14:30:09 +0200

    styled heading and property info box

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ======== DOM caching =======
 const colOne    = document.querySelector('input[name="color1"]'),
     colTwo      = document.querySelector('input[name="color2"]'),
-    colorsBox   = document.querySelector('.colors'),
+    settingsBox = document.querySelector('.settings'),
     colValueBox = document.querySelector('h3'),
-    bodyBox     = document.querySelector('body');
+    bodyBox     = document.querySelector('body');  
 
 // ======= Functions (i.a. event handlers) ========
 function setColor(evt) {
@@ -15,4 +15,4 @@ function setColor(evt) {
 setColor();
 
 // ========= Event listeners ==========
-colorsBox.addEventListener('input', setColor);
+settingsBox.addEventListener('input', setColor);
